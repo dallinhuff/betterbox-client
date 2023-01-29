3f7abc08678406d53b6302cb79bcf6379377fbfc
dhuff <dallinhuff@gmail.com>
2023-01-28 17:29:06 -0700

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -6,17 +6,16 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+    const isLoadingComplete = useCachedResources();
+    const colorScheme = useColorScheme();
+    if (!isLoadingComplete) {
+        return null;
+    } else {
+        return (
+            <SafeAreaProvider>
+                <Navigation colorScheme={colorScheme} />
+                <StatusBar />
+            </SafeAreaProvider>
+        );
+    }
 }
--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -10,27 +10,20 @@ import * as Linking from 'expo-linking';
 import { RootStackParamList } from '../types';
 
 const linking: LinkingOptions<RootStackParamList> = {
-  prefixes: [Linking.createURL('/')],
-  config: {
-    screens: {
-      Root: {
+    prefixes: [Linking.createURL('/')],
+    config: {
         screens: {
-          TabOne: {
-            screens: {
-              TabOneScreen: 'one',
+            Root: {
+                screens: {
+                    TabOne: { screens: { TabOneScreen: 'one' } },
+                    TabTwo: { screens: { TabTwoScreen: 'two' } },
+                    TabThree: { screens: { TabThreeScreen: 'three' } }
+                }
             },
-          },
-          TabTwo: {
-            screens: {
-              TabTwoScreen: 'two',
-            },
-          },
-        },
-      },
-      Modal: 'modal',
-      NotFound: '*',
-    },
-  },
+            Modal: 'modal',
+            NotFound: '*',
+        }
+    }
 };
 
 export default linking;
