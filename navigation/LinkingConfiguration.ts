5ac18339fb64f83517ebb82dfc8960dd801da881
dhuff <dallinhuff@gmail.com>
2023-01-28 18:30:15 -0700

    whitespace fixes (spaces -> tabs)

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -10,20 +10,20 @@ import * as Linking from 'expo-linking';
 import { RootStackParamList } from '../types';
 
 const linking: LinkingOptions<RootStackParamList> = {
-    prefixes: [Linking.createURL('/')],
-    config: {
-        screens: {
-            Root: {
-                screens: {
-                    TabOne: { screens: { TabOneScreen: 'one' } },
-                    TabTwo: { screens: { TabTwoScreen: 'two' } },
-                    TabThree: { screens: { TabThreeScreen: 'three' } }
-                }
-            },
-            Modal: 'modal',
-            NotFound: '*',
-        }
-    }
+	prefixes: [Linking.createURL('/')],
+	config: {
+		screens: {
+			Root: {
+				screens: {
+					TabOne: { screens: { TabOneScreen: 'one' } },
+					TabTwo: { screens: { TabTwoScreen: 'two' } },
+					TabThree: { screens: { TabThreeScreen: 'three' } }
+				}
+			},
+			Modal: 'modal',
+			NotFound: '*',
+		}
+	}
 };
 
 export default linking;
