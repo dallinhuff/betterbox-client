72058ec716deb5b9f722f080dc93d9122557a717
Lance Meurs <lmeurs728@gmail.com>
2023-03-27 14:26:24 -0600

    Temporarily fix test button

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -3,8 +3,7 @@ import { StyleSheet, Pressable, Image } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App';
-import DropShadow from "react-native-drop-shadow";
-
+import DropShadow from 'react-native-drop-shadow';
 
 type ProfileScreenNavigationProp = NativeStackNavigationProp<
 	RootStackParamList,
@@ -15,45 +14,84 @@ type Props = {
 	navigation: ProfileScreenNavigationProp;
 };
 
-
 export default function Home({ navigation }: Props) {
-
 	const register = () => {
-		navigation.navigate('Register')
+		navigation.navigate('Register');
 	};
 
 	const login = () => {
-		navigation.navigate('Login')
+		navigation.navigate('Login');
 	};
 
 	const test = () => {
-		navigation.navigate('MainPages')
+		navigation.navigate('MainPages', { authToken: '' });
 	};
 
 	return (
 		<View style={styles.container}>
 			<Image
 				style={styles.film}
-				source={require("../assets/images/film-icon.png")}></Image>
-			<Text style={{ fontSize: "24", marginBottom: 10, color: '#707070', marginTop: -300, }}>Welcome to</Text>
-			<Text style={{ fontSize: "48", marginBottom: 10, color: '#707070' }}>BETTERBOX</Text>
-			<Text style={{ fontSize: "16", marginBottom: 10, color: '#707070' }}>The social movie rating app.</Text>
+				source={require('../assets/images/film-icon.png')}
+			></Image>
+			<Text
+				style={{
+					fontSize: '24',
+					marginBottom: 10,
+					color: '#707070',
+					marginTop: -300,
+				}}
+			>
+				Welcome to
+			</Text>
+			<Text style={{ fontSize: '48', marginBottom: 10, color: '#707070' }}>
+				BETTERBOX
+			</Text>
+			<Text style={{ fontSize: '16', marginBottom: 10, color: '#707070' }}>
+				The social movie rating app.
+			</Text>
 			<DropShadow style={styles.shadowProp}>
 				<Pressable style={styles.getStarted} onPress={() => register()}>
-					<Text style={{ fontSize: "24", marginBottom: 10, marginTop: 5, color: '#fffcf2' }}>Get Started</Text>
+					<Text
+						style={{
+							fontSize: '24',
+							marginBottom: 10,
+							marginTop: 5,
+							color: '#fffcf2',
+						}}
+					>
+						Get Started
+					</Text>
 				</Pressable>
 			</DropShadow>
 
-
 			{/**
 			 * Needed to get to profile page for now. Delete if you need to.
-			*/}
+			 */}
 			<Pressable onPress={() => test()}>
-				<Text style={{ fontSize: "24", marginBottom: 5, marginTop: 5, color: '#707070' }}>Test</Text>
+				<Text
+					style={{
+						fontSize: '24',
+						marginBottom: 5,
+						marginTop: 5,
+						color: '#707070',
+					}}
+				>
+					Test
+				</Text>
 			</Pressable>
 
 			<Pressable onPress={() => login()}>
-				<Text style={{ fontSize: "11", marginTop: 10, color: "#707070", textDecorationLine: 'underline', marginBottom: 100, }}>I ALREADY HAVE AN ACCOUNT</Text>
+				<Text
+					style={{
+						fontSize: '11',
+						marginTop: 10,
+						color: '#707070',
+						textDecorationLine: 'underline',
+						marginBottom: 100,
+					}}
+				>
+					I ALREADY HAVE AN ACCOUNT
+				</Text>
 			</Pressable>
 		</View>
 	);
@@ -96,5 +134,5 @@ const styles = StyleSheet.create({
 		height: 80,
 		resizeMode: 'contain',
 		marginTop: -200,
-	}
+	},
 });
